11acd76e94cc8939d44ac1fe693db6177d5210b7
MNPCMW6444 <30693526+MNPCMW6444@users.noreply.github.com>
2021-11-19 20:06:14 +0200

    bkp

--- routers/snippetRouter.js
+++ routers/snippetRouter.js
@@ -44,6 +44,8 @@ router.put("/:id", auth, async (req, res) => {
   try {
     const { title } = req.body;
     const snippetId = req.params.id;
+    console.log(snippetId);
+    console.log(title);
 
     // validation
 
@@ -109,19 +111,43 @@ router.put("/check/:id", auth, async (req, res) => {
 
     originalSnippet.done = done;
 
-    const savedSnippet = await originalSnippet.save();
+    let savedSnippet = await originalSnippet.save();
 
     const allsnippets = await Snippet.find({ user: req.user });
 
-    if (done) {
-      allsnippets.forEach(async (snippet) => {
+    const checkrec = async (snippet, log) => {
+      if (snippet) {
         const originalSnippet2 = await Snippet.findById(snippet._id);
-        if (originalSnippet2.parent === savedSnippet._id) {
-          console.log("asdasd");
-          originalSnippet2.done = true;
-          await originalSnippet2.save();
+        if (originalSnippet2) {
+          if (log) {
+            console.log(JSON.stringify(savedSnippet._id));
+            console.log(JSON.stringify(originalSnippet2.parent));
+            console.log("  ");
+          }
+          if (
+            JSON.stringify(savedSnippet._id) ===
+            JSON.stringify(originalSnippet2.parent)
+          ) {
+            console.log("CHKCHKCHK");
+            originalSnippet2.done = true;
+            savedSnippet = await originalSnippet2.save();
+          } else if (originalSnippet2 && originalSnippet2.parent) {
+            const id = JSON.stringify(originalSnippet2.parent);
+            const pd = '"6197d73f8c55270844fb0a01"';
+            const log = id === pd ? true : false;
+            /*  console.log("id IS " + id);
+            console.log("pd IS " + pd);
+            console.log("log IS " + log);
+            console.log(); */
+            checkrec(await Snippet.findById(originalSnippet2.parent), log);
+          }
         }
-      });
+      }
+    };
+
+    if (done) {
+      for (let i = 0; i < allsnippets.length; i++)
+        checkrec(allsnippets[i], false);
     }
 
     res.json(savedSnippet);
